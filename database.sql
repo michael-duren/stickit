2e676aa1390b7f3e35fbcb99a0a5372add916ebf
Michael Duren <michaeld@michaelduren.com>
2023-09-02 09:20:41 -0500

    Update to db file
    Redid documentation.

--- database.sql
+++ database.sql
@@ -1,21 +1,24 @@
 -- This file is used to create the database and tables
-
 -- DROP statements for testing purposes
--- DROP TABLE IF EXISTS EXERCISES;
--- DROP TABLE IF EXISTS USERS;
--- DROP TABLE IF EXISTS USER_EXERCISES_COMPLETED;
+DROP TABLE IF EXISTS USER_EXERCISES_COMPLETED_NOTES;
 
+DROP TABLE IF EXISTS USER_EXERCISES_COMPLETED;
+
+DROP TABLE IF EXISTS "user";
+
+DROP TABLE IF EXISTS EXERCISES;
 
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "USER" (
-    "ID" SERIAL PRIMARY KEY,
-    "USERNAME" VARCHAR (80) UNIQUE NOT NULL,
-    "PASSWORD" VARCHAR (1000) NOT NULL
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL
 );
 
+-- table for all exercises
 CREATE TABLE EXERCISES (
     ID SERIAL PRIMARY KEY,
     NAME VARCHAR(100) NOT NULL,
@@ -27,18 +30,27 @@ CREATE TABLE EXERCISES (
     INSTRUMENT VARCHAR(100),
     TEACHER VARCHAR(100),
     DESCRIPTION TEXT NOT NULL,
-    DIRECTIONS VARCHAR(512)[] NOT NULL,
-    REMEMBER VARCHAR(512)[],
+    DIRECTIONS VARCHAR(512) [] NOT NULL,
+    REMEMBER VARCHAR(512) [],
     MINIMUM_TIME_MINUTES DECIMAL NOT NULL,
     BPM_RANGE VARCHAR(100) NOT NULL,
     VIDEO_LINK VARCHAR(1096)
 );
 
+-- When a user completes an exercise it will be added here
 CREATE TABLE USER_EXERCISES_COMPLETED (
     ID SERIAL PRIMARY KEY,
-    USER_ID INT REFERENCES "USER" ON DELETE CASCADE,
+    USER_ID INT REFERENCES "user" ON DELETE CASCADE,
     EXERCISE_ID INT REFERENCES EXERCISES ON DELETE CASCADE,
-    EXERCISE_NOTES TEXT []
+    COMPLETED_AT DATE NOT NULL
+);
+
+-- A users notes on a exercise they completed. Includes the date
+CREATE TABLE USER_EXERCISES_COMPLETED_NOTES (
+    ID SERIAL PRIMARY KEY,
+    EXERCISE_NOTE TEXT NOT NULL,
+    USER_EXERCISES_COMPLETED_ID INT REFERENCES USER_EXERCISES_COMPLETED ON DELETE CASCADE,
+    CREATED_AT DATE NOT NULL
 );
 
 -- EXERCISE GIVEN BY LUKE
@@ -92,4 +104,31 @@ that''s okay. It means you are not gripping the stick too hard."}',
         5.0,
         '80 - 100 BPM',
         null
+    );
+
+-- Dummy Data
+INSERT INTO
+    "user" ("username", "password")
+VALUES
+    (
+        'michael',
+        '$2a$10$lm0PBXwkXLo.LDNthIwaL.GlgomiP6M49nodueisQjnFTmOTgk7ra'
+    );
+
+INSERT INTO
+    USER_EXERCISES_COMPLETED (USER_ID, EXERCISE_ID, COMPLETED_AT)
+VALUES
+    (1, 1, NOW());
+
+INSERT INTO
+    USER_EXERCISES_COMPLETED_NOTES (
+        EXERCISE_NOTE,
+        USER_EXERCISES_COMPLETED_ID,
+        CREATED_AT
+    )
+VALUES
+    (
+        'Awesome exercise, I felt I could have done better in several areas though.',
+        1,
+        NOW()
     );
