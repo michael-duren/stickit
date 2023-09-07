0b57d2c82cb9744c05d99112cd98d8c150f33840
Eric Hines <hines.eric.m@gmail.com>
2023-09-06 20:45:27 -0500

    Closes #3 Completed GET request for /api/exercises: SELECT * FROM EXERCISES WHERE ID = $1; tested successfully in Postman

--- server/routes/exercises.router.js
+++ server/routes/exercises.router.js
@@ -5,10 +5,10 @@ const userStrategy = require('../strategies/user.strategy');
 const router = express.Router();
 
 //GET method for reading an exercise by its ID
-router.get('/exercises', (req, res) => {
-  let requestExercise = 'SELECT * FROM "EXERCISES" WHERE ID = $1;';
+router.get('/:id', (req, res) => {
+  let requestExercise = 'SELECT * FROM EXERCISES WHERE ID = $1;';
 
-  pool.query(requestExercise, [req.exercises.id])
+  pool.query(requestExercise, [req.params.id])
   .then((result) => {
     res.status(200).send(result.rows);
   })
@@ -17,8 +17,6 @@ router.get('/exercises', (req, res) => {
     res.sendStatus(500)
   });
 });
-
-
 
 module.exports = router;
 
