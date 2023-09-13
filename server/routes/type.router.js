1976f6c15e14dcae63a8a7d568217dccfc2a01bc
Tyler Burns <Tburnsie10@gmail.com>
2023-09-13 13:51:09 -0500

    corrected type.router.js to make it get all types

--- server/routes/type.router.js
+++ server/routes/type.router.js
@@ -4,18 +4,18 @@ const userStrategy = require('../strategies/user.strategy');
 
 const router = express.Router();
 
-//GET method for reading an exercise by its ID
-router.get('/:id', (req, res) => {
-  let requestTypes = 'SELECT * FROM TYPE WHERE ID = $1;';
+// GET method for getting all types
+router.get('/', (req, res) => {
+  let requestTypes = 'SELECT * FROM TYPE;';
 
-  pool.query(requestTypes, [req.params.id])
-  .then((result) => {
-    res.status(200).send(result.rows);
-  })
-  .catch((error) => {
-    console.log('Error with get types request:', error);
-    res.sendStatus(500)
-  });
+  pool.query(requestTypes)
+    .then((result) => {
+      res.status(200).send(result.rows);
+    })
+    .catch((error) => {
+      console.log('Error with get types request:', error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
