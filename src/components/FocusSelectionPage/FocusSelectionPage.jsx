aef49b9a1145bf062f68bbb89edba517fbabb798
Tyler Lowe <tlowe3420@gmail.com>
2023-09-13 16:22:02 -0500

    updated layout of focus selection

--- src/components/FocusSelectionPage/FocusSelectionPage.jsx
+++ src/components/FocusSelectionPage/FocusSelectionPage.jsx
@@ -1,6 +1,9 @@
 import { useParams } from 'react-router-dom';
 import NotFound from '../NotFoundPage/NotFoundPage';
 import { useEffect, useState } from 'react';
+import Grid from "@mui/material/Grid";
+import SideNav from '../SideNav/SideNav';
+import Button from "@mui/material/Button";
 import './FocusSelectionPage.css';
 
 export default function FocusSelectionPage() {
@@ -50,23 +53,34 @@ export default function FocusSelectionPage() {
   }, []);
 
   return (
-    <div className="flex container gap-16 flex-col items-center">
-      <h2>What specifically would you like to work on for {type}</h2>
-      <div className="flex m-b-xl gap-16 flex-col h-full items-center">
-        {focuses.map((focus) => {
-          return (
-            <div key={focus.id}>
-              <button className="btn-focus-select">{focus.name}</button>
+    <div className="background-primary-grey">
+      <Grid className="main-page-padding">
+        <Grid container>
+          <Grid item md={1} lg={1}>
+            <SideNav />
+          </Grid>
+          <Grid item  md={11} lg={11}>
+            <div className="main-page-content-container">
+              <Grid item  lg={3}>
+                <h2 className='text-center'>What specifically would you like to work on for {type}</h2>
+                <div className="flex m-b-xl gap-16 flex-col items-center">
+                  {focuses.map((focus) => {
+                    return (
+                      <div key={focus.id}>
+                        <Button className="btn-focus-select">{focus.name}</Button>
+                      </div>
+                    );
+                  })}
+                </div>
+                <Button fullWidth variant="contained" className="btn" type="submit" name="submit" value="Sign In" >Next</Button>
+                <div>
+                  <button className='m-t-xl empty-button primary-blue'>Back</button>
+                </div>
+              </Grid>
             </div>
-          );
-        })}
-      </div>
-      <div className="flex items-center flex-col gap-16 w-full">
-        <button className="w-half btn">Next</button>
-        <div className="w-half">
-          <button className="border-none text-blue">Back</button>
-        </div>
-      </div>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
