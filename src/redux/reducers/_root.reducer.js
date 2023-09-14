cc6477ac12437609dc2bb462db2edf2df8fb2485
Michael Duren <michaeld@michaelduren.com>
2023-09-13 19:45:37 -0500

    Created TypeSelectionPage, fix .sql script, setup nav and fix redux store

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
-import sessionFormReducer from './session-form.reducer';
-import sessionReducer from './session.reducer';
+import sessionForm from './session-form.reducer';
+import session from './session.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -13,8 +13,8 @@ import sessionReducer from './session.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  sessionFormReducer,
-  sessionReducer,
+  sessionForm,
+  session,
 });
 
 export default rootReducer;
