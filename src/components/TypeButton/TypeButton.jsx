0055b3a7531073af23e904403828ccbca4c3d76d
Tyler Burns <Tburnsie10@gmail.com>
2023-09-24 20:55:05 -0500

    made buttons toggle and clickable with red background, as the blue background was conflicting with the color of the icons and making them disappear

--- src/components/TypeButton/TypeButton.jsx
+++ src/components/TypeButton/TypeButton.jsx
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import Grid from "@mui/material/Grid";
-import "./TypeButton.css";
-import { ReactComponent as Rabbit } from '../../images/rabbitIcon.svg';
-import { ReactComponent as Innovation } from '../../images/innovationIcon.svg';
-import { ReactComponent as ABC } from '../../images/abcIcon.svg';
-import { ReactComponent as Target } from '../../images/targetIcon.svg';
+import './TypeButton.css';
 
+function TypeButton({ icon, label }) {
+  // Define isActive as a state variable
+  const [isActive, setIsActive] = useState(false);
 
-
-
-
-function TypeButton({icon, label}) {
+  const handleClick = () => {
+    // Toggle the isActive state
+    setIsActive(!isActive);
+  };
 
   return (
-    <button className="type-button">
-      {icon}  
+    <button 
+      className={isActive ? "type-button active" : "type-button"}
+      onClick={handleClick}
+    >
+      {icon}
       <div>{label}</div>
     </button>
   );
-
 }
 
 export default TypeButton;
-
-//script tag that imports
-//
