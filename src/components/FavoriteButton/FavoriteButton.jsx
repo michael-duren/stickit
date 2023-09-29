a09e4d6d584a59d32f322eba1262d2e6a2fe6f93
Michael Duren <michaeld@michaelduren.com>
2023-09-29 12:37:52 -0500

    heart animation works for exercise

--- src/components/FavoriteButton/FavoriteButton.jsx
+++ src/components/FavoriteButton/FavoriteButton.jsx
@@ -1,5 +1,4 @@
-import { ReactComponent as HeartIconFill } from '../../images/heart.svg';
-import { ReactComponent as HeartIconOutline } from '../../images/heart-outline.svg';
+import { ReactComponent as HeartOutlineIcon } from '../../images/heart-outline.svg';
 import { useEffect, useState } from 'react';
 import './FavoriteButton.css';
 import toast from 'react-hot-toast';
@@ -9,13 +8,28 @@ export default function FavoriteButton({ exercise }) {
   const { userExerciseDetails } = useSelector(
     (store) => store.sessionUserDetails
   );
+  const [isShaking, setIsShaking] = useState(false);
   const [isHearted, setIsHearted] = useState(userExerciseDetails.hearted);
+
+  useEffect(() => {
+    if (isShaking) {
+      const animationDuration = 1000;
+      const timer = setTimeout(() => {
+        setIsShaking(false);
+      }, animationDuration);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [isShaking]);
 
   useEffect(() => {
     setIsHearted(userExerciseDetails.hearted);
   }, [userExerciseDetails]);
 
   const handleHeartClick = async () => {
+    setIsShaking(true);
     if (isHearted) {
       try {
         const result = await fetch(`/api/user/exercises/heart/${exercise.id}`, {
@@ -24,7 +38,10 @@ export default function FavoriteButton({ exercise }) {
         });
 
         if (result.status !== 204) toast.error('Could not dislike exercise.');
-        if (result.status === 204) setIsHearted(false);
+        if (result.status === 204) {
+          setIsHearted(false);
+          toast.success('Exercise removed from favorites.');
+        }
       } catch (error) {
         console.error(`Error in handleHeartClick: ${error}`);
         toast.error('Could not dislike exercise.');
@@ -35,7 +52,10 @@ export default function FavoriteButton({ exercise }) {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
         });
-        if (result.status === 201) setIsHearted(true);
+        if (result.status === 201) {
+          setIsHearted(true);
+          toast.success('Exercise added to favorites.');
+        }
       } catch (error) {
         console.error(`Error in handleHeartClick: ${error}`);
         toast.error('Could not like exercise');
@@ -46,13 +66,14 @@ export default function FavoriteButton({ exercise }) {
   return (
     <button
       onClick={handleHeartClick}
-      className="heart-button transform-all duration-300"
+      className={`${isShaking ? 'animate-shake' : ''} heart-button`}
     >
-      {isHearted ? (
-        <HeartIconFill className={'heart-button-fill'} />
-      ) : (
-        <HeartIconOutline className={'heart-button-outline'} />
-      )}
+      <HeartOutlineIcon
+        className={`${
+          isHearted ? 'heart-button-fill' : 'heart-button-outline'
+        } heart-button-icon`}
+      />
+      {/* <HeartIconOutline className={'heart-button-outline'} /> */}
     </button>
   );
 }
