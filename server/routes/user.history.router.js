d969c603639f2af0ea9d13f7b91bd022cede9aa4
Michael Duren <michaeld@michaelduren.com>
2023-09-30 15:24:40 -0500

    create endpoints for user session history

--- server/routes/user.history.router.js
+++ server/routes/user.history.router.js
@@ -5,23 +5,100 @@ const router = express.Router();
 
 // get exercises for this week
 router.get('/week', (req, res) => {
-  const exerciseId = req.params.exerciseid;
-  const sessionId = req.params.sessionid;
   const userId = req.user.id;
-  const userExerciseQuery = `
-      SELECT E.*, F.exercise_id as hearted
-      FROM user_session_exercises as E
-      LEFT JOIN user_favorite_exercises as F
-      ON E.exercise_id = F.exercise_id
-      WHERE E.exercise_id = $1 AND E.user_id = $2 AND E.session_id = $3;
+  const userCurrentWeekSessionsQuery = `
+    SELECT S.*, COUNT(E) AS total_exercises 
+    FROM user_sessions AS S
+    JOIN user_session_exercises AS E
+    ON S.id = E.session_id
+    WHERE S.user_id = $1
+    GROUP BY S.id
+    HAVING S.completed_at >= NOW() - INTERVAL '7 DAYS'
+    ORDER BY S.completed_at DESC;
     `;
   pool
-    .query(userExerciseQuery, [exerciseId, userId, sessionId])
+    .query(userCurrentWeekSessionsQuery, [userId])
     .then((result) => {
-      res.send(result.rows[0]);
+      res.send(result.rows);
     })
     .catch((error) => {
-      console.log('Error with get exercises request:', error);
+      console.error('Error getting user sessions', error);
+      res.sendStatus(500);
+    });
+});
+
+// get exercises for the current month
+router.get('/month', (req, res) => {
+  const userId = req.user.id;
+  const userCurrentSessionMonthQuery = `
+    SELECT S.*, COUNT(E) AS total_exercises 
+    FROM user_sessions AS S
+    JOIN user_session_exercises AS E
+    ON S.id = E.session_id
+    WHERE S.user_id = $1
+    GROUP BY S.id
+    HAVING
+    EXTRACT(MONTH from S.completed_at) = EXTRACT(MONTH FROM NOW())
+    AND
+    EXTRACT(YEAR FROM S.completed_at) = EXTRACT(YEAR FROM NOW())
+    ORDER BY S.completed_at DESC;
+    `;
+  pool
+    .query(userCurrentSessionMonthQuery, [userId])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.error('Error getting user sessions', error);
+      res.sendStatus(500);
+    });
+});
+
+// get exercises for the current year
+router.get('/year', (req, res) => {
+  const userId = req.user.id;
+  const userCurrentSessionYearQuery = `
+    SELECT S.*, COUNT(E) AS total_exercises 
+    FROM user_sessions AS S
+    JOIN user_session_exercises AS E
+    ON S.id = E.session_id
+    WHERE S.user_id = $1
+    GROUP BY S.id
+    HAVING S.completed_at >= NOW() - INTERVAL '1 YEAR'
+    ORDER BY S.completed_at DESC;
+  `;
+
+  pool
+    .query(userCurrentSessionYearQuery, [userId])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.error('Error getting user sessions', error);
+      res.sendStatus(500);
+    });
+});
+
+// get exercises for all time
+router.get('/all', (req, res) => {
+  const userId = req.user.id;
+  const userAllSessionsQuery = `
+    SELECT S.*, COUNT(E) AS total_exercises
+    FROM user_sessions AS S
+    JOIN user_session_exercises AS E
+    ON S.id = E.session_id
+    WHERE S.user_id = $1
+    GROUP BY S.id
+    ORDER BY S.completed_at DESC;
+  `;
+
+  pool
+    .query(userAllSessionsQuery, [userId])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.error('Error getting user sessions', error);
       res.sendStatus(500);
     });
 });
